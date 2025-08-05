067a03a07f846e25694dc758202799f03fca3102
Vlad <119672242+vlaferjr@users.noreply.github.com>
2025-08-05 18:14:16 -0300

    construção do banner, home e definição de rota para home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',//quando usuario acessar a rota raiz, ele será redirecionado para a página home
+    component: HomeComponent //componente que será carregado quando o usuário acessar a rota raiz
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent {
+  /*propriedades que serão recebidas do componente pai, o componente pai é o home.component.html
+  o banner vai receber a imagem e o texto alternativo, o banner vai ser reutilizado em outros lugares,
+  então é interessante que ele receba as informações de forma dinâmica para isso, usamos o decorator @Input*/
+  @Input() src: string = ''; //caminho da imagem
+  @Input() alt: string = ''; //texto alternativo
 
 }
